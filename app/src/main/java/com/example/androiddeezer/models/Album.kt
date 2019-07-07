2b5b536833259e7b405b2c587ee6c59eaa1b920f
PicoisCharlotte <charlotte.picois.sio@gmail.com>
2019-07-07 15:22:52 +0200

    Add artist & album JSON constructor + build.gradle add glide

--- app/src/main/java/com/example/androiddeezer/models/Album.kt
+++ app/src/main/java/com/example/androiddeezer/models/Album.kt
@@ -1,10 +1,32 @@
 package com.example.androiddeezer.models
 
+import org.json.JSONObject
+
 class Album {
-    private var id: Int? = null
+    private var id: String? = null
     private var title: String? = null
     private var cover_small: String? = null
     private var genre_id: Int? = null
     private var duration: Int? = null
     private var artist: Artist? = null
+
+
+    constructor(JObject: JSONObject){
+        if(JObject.has("id"))
+            this.id = JObject.getString("id")
+        if(JObject.has("title"))
+            this.title = JObject.getString("title")
+        if(JObject.has("cover_small"))
+            this.cover_small = JObject.getString("cover_small")
+        if(JObject.has("duration"))
+            this.duration = JObject.getInt("duration")
+        if(JObject.has("artist")){
+            var artistJSON = JObject.getJSONObject("artist")
+            artist = Artist(artistJSON)
+        }
+    }
+
+    public fun getCoverSmall(): String? {
+        return cover_small
+    }
 }
--- app/src/main/java/com/example/androiddeezer/models/Artist.kt
+++ app/src/main/java/com/example/androiddeezer/models/Artist.kt
@@ -1,5 +1,12 @@
 package com.example.androiddeezer.models
 
+import org.json.JSONObject
+
 class Artist {
     private var name: String? = null
+
+    constructor(JObject: JSONObject){
+        if(JObject.has("name"))
+            name = JObject.getString("name")
+    }
 }
