58685a6dc0c7c519340679c4b4cce09e268b31bf
Lukile <lucile.1988.ls@gmail.com>
2019-07-12 15:43:48 +0200

    Deleted No tracks Fragment creation, just go back to album list when no tracks are found

--- app/src/main/java/com/example/androiddeezer/fragments/ListTracksFragment.kt
+++ app/src/main/java/com/example/androiddeezer/fragments/ListTracksFragment.kt
@@ -1,29 +1,26 @@
 package com.example.androiddeezer.fragments
 
 import android.content.Context
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.androiddeezer.R
-import com.example.androiddeezer.adapters.AlbumAdapter
 import com.example.androiddeezer.adapters.TrackAdapter
 import com.example.androiddeezer.models.Album
 import com.example.androiddeezer.models.Tracks
 import com.squareup.picasso.Picasso
-import interfaces.AdapaterCallbackTracksNotFound
 import kotlinx.android.synthetic.main.fragment_list_tracks.*
 import okhttp3.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 
-class ListTracksFragment :Fragment(), AdapaterCallbackTracksNotFound {
+class ListTracksFragment :Fragment() {
     private val client = OkHttpClient()
 
     var trackList: MutableList<Tracks> = mutableListOf()
@@ -31,13 +28,13 @@ class ListTracksFragment :Fragment(), AdapaterCallbackTracksNotFound {
     var url = "http://api.deezer.com/2.0/album/$albumId/tracks"
 
     lateinit var trackAdapter: TrackAdapter
+
     private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         trackAdapter = TrackAdapter(context)
         linearLayoutManager = LinearLayoutManager(context)
-
 
         return inflater.inflate(R.layout.fragment_list_tracks, container, false)
     }
@@ -66,13 +63,14 @@ class ListTracksFragment :Fragment(), AdapaterCallbackTracksNotFound {
         }
     }
     fun getTracks(url: String) {
+        var responseData: String? = null
         val request = Request.Builder()
             .url(url)
             .build()
         client.newCall(request).enqueue(object: Callback {
             override fun onFailure(call: Call, e : IOException) {}
             override fun onResponse(call: Call, response: Response) {
-                val responseData = response.body?.string()
+                responseData = response.body?.string()
 
                 try {
                     val json = JSONObject(responseData)
@@ -98,8 +96,9 @@ class ListTracksFragment :Fragment(), AdapaterCallbackTracksNotFound {
                 } catch (e: JSONException) {
                     activity.runOnUiThread {
                         context.toast("No datas found for this tracklist")
+
+                        goBackToAlbumList()
                     }
-                    onClickItem()
                     e.printStackTrace()
                 }
 
@@ -111,18 +110,13 @@ class ListTracksFragment :Fragment(), AdapaterCallbackTracksNotFound {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
-    override fun onClickItem() {
-        println("No datas found")
-        val fragment = NoTracksFoundFragment.newInstance()
-        if(!fragment.isAdded) {
-            val transaction = fragmentManager.beginTransaction()
+    fun goBackToAlbumList() {
+        val fragment = ListAlbumsFragment.newInstance()
 
-            transaction.replace(R.id.content, fragment)
-            transaction.remove(this)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-        return
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.content, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
-
 }
--- app/src/main/java/interfaces/AdapaterCallbackTracksNotFound.kt
+++
@@ -1,5 +0,0 @@
-package interfaces
-
-interface AdapaterCallbackTracksNotFound {
-    fun onClickItem()
-}
